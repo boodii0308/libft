6caa2c1ee39d87acd77bb0b35c900bd13c512860
Tengisbold Batsaikhan <tebatsai@e1z2r5p7.42.us.org>
2019-05-13 22:46:26 -0700

    hey

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: tebatsai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 13:03:37 by tebatsai          #+#    #+#             */
-/*   Updated: 2019/05/11 18:20:00 by tebatsai         ###   ########.fr       */
+/*   Updated: 2019/05/13 22:45:31 by tebatsai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,27 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
 	size_t		j;
 	size_t		i;
-	const char	*c;
+	size_t		m;
 
 	i = 0;
-	c = s2;
+	m = -1;
 	if (*s2 == '\0')
 		return ((char *)s1);
 	else if (*s1 == '\0')
 		return (NULL);
-	while (s1[i] && i < n)
+	while (s1[++m] && i < n)
 	{
 		j = 0;
+		i = m;
 		while (s1[i] == s2[j] && i < n)
 		{
-			j++;
 			i++;
+			j++;
 			if (s2[j] == '\0')
 				return (char *)&s1[i - j];
 			else if (i == n)
 				return (NULL);
 		}
-		i++;
 	}
 	return (NULL);
 }
