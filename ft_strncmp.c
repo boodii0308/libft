8d63dcba4de1c4f36af83f57d6a9d3b9c22027e4
Tengisbold Batsaikhan <tebatsai@e1z2r5p7.42.us.org>
2019-05-13 23:06:17 -0700

    lk

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tebatsai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 15:03:04 by tebatsai          #+#    #+#             */
-/*   Updated: 2019/05/13 15:38:04 by tebatsai         ###   ########.fr       */
+/*   Updated: 2019/05/13 23:06:04 by tebatsai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,11 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 		else if (*s1 == '\0')
 			return (0);
 	}
-	if (*s1 == '\0' || *s2 == '\0')
+	if (n == 0)
+		return (0);
+	else if (*s1 != *s2)
+		return (*(unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1);
+	else if (*s1 == '\0' || *s2 == '\0')
 		return (*(unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1);
 	return (0);
 }
