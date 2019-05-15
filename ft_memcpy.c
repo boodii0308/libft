6e507839d3370f1ef7d18dfc2a97154f853455ab
Tengisbold Batsaikhan <tebatsai@e1z2r5p7.42.us.org>
2019-05-14 22:27:24 -0700

    aa

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tebatsai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 16:12:57 by tebatsai          #+#    #+#             */
-/*   Updated: 2019/05/14 21:52:55 by tebatsai         ###   ########.fr       */
+/*   Updated: 2019/05/14 22:27:05 by tebatsai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ void	*ft_memcpy(void *str1, const void *str2, size_t n)
 		p[i] = s[i];
 		i++;
 	}
-	str1 = (void *)dest;
+	str1 = (void *)p;
 	return (str1);
 }
