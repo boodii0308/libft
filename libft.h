ae1458faa88a2f1cfdb419c46d6fcf8333ad9855
Tengisbold Batsaikhan <tebatsai@e1z2r5p7.42.us.org>
2019-05-14 23:06:15 -0700

    LAST

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tebatsai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 12:34:08 by tebatsai          #+#    #+#             */
-/*   Updated: 2019/05/14 22:56:00 by tebatsai         ###   ########.fr       */
+/*   Updated: 2019/05/14 23:05:57 by tebatsai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ typedef struct		s_list
 	struct s_list	*next;
 }					t_list;
 
+
 int					ft_atoi(const char *s);
 char				*ft_itoa(int n);
 void				ft_bzero(void *s, size_t n);
@@ -41,7 +42,6 @@ void				*ft_memchr(const void *str, int c, size_t n);
 int					ft_memcmp(const void *str1, const void *str2, size_t n);
 void				*ft_memcpy(void *str1, const void *str2, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
-char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void				*ft_memset(void *ptr, int value, size_t num);
 void				ft_putnbr(int n);
 char				*ft_strcat(char *dest, const char *src);
